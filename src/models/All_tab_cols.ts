46a47c6f0d862f17c5249def5b0d4cfabe8c83d1
irissun9602 <iris9602@naver.com>
2018-08-29 02:27:43 +0900

    feat: 서버 연결 서비스 구현

---
+++ src/models/All_tab_cols.ts
@@ -0,0 +1,6 @@
+export class All_tab_cols{
+    column_id : number;
+	column_name : string;
+	data_type : string;
+
+}
---
+++ src/services/meta.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { All_tab_cols } from '../models/All_tab_cols';
+@Injectable()
+export class MetaService{
+  private URL = 'http://localhost:8080/data_L/api/';
+   private http: Http; 
+   constructor(http: Http) { 
+     this.http = http; 
+    } 
+    
+    getColumns(): Promise<All_tab_cols[]> { 
+      return this.http.get(this.URL + "columns")
+      .toPromise()
+      .then(response => response.json() as All_tab_cols[])
+      .catch(this.handleError); 
+    }
+
+    private handleError(error: any): Promise<any> { 
+      console.error('An error occurred', error); // for demo purposes only 
+      return Promise.reject(error.message || error);
+     }
+}
